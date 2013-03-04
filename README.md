59dfa92f02d65caf6bb7bd77331e29c09b9c1603
Steve Kinney <hello@stevekinney.net>
2013-03-04 11:01:58 -0500

    Initial commit FTW

---
+++ README.md
@@ -0,0 +1,63 @@
+# Phone Number Formatter
+
+Parsing and formatting phone numbers so you don't have to.
+
+## Getting Started
+Install the module with: `npm install phone-formatter`
+
+Phone-formatter is pretty straight-forward. First, it can normalize pretty much any format you can throw at it. If it can't, then send a pull request with a failing test and it will (shortly thereafter).
+
+```javascript
+var phoneFormatter = require('phone-formatter');
+
+phoneFormatter.normalize('212.555.1212');
+// returns "2125551212"
+
+phoneFormatter.normalize('+1 (212) 555-1212');
+// returns "2125551212"
+```
+
+You get the idea. It can also format a series of ten digits into almost any format your heart desires. Use the letter "N" as a place holder.
+
+```javascript
+phoneFormatter.format("2125551212", "(NNN) NNN-NNNN");
+// returns "(212) 555-1212"
+```
+
+## Documentation
+
+As it stands, there are only two methods, `normalize` and `format`. They are pretty much fleshed out above. That said, I'm reserving this second for future greatness.
+
+I can confirm that Phone Formatter can normalize the following formats.
+
+* (212) 555 1212
+* (212) 555.1212
+* (212) 555-1212
+* (212) 5551212
+* (212)5551212
+* 212 555 1212
+* 212.555.1212
+* 212-555-1212
+* 1-212-555-1212
+* +1 (212) 555-1212
+* 12125551212
+* +45 (212) 555-1212
+* 212555121
+
+It may handle ever more, but I haven't tested it.
+
+## Contributing
+
+Pull requests are welcome as long as they are accompanied by tests.
+
+Right now, this library is incredibly American-centric and that kind of stinks, but my use case consisted exclusively of American phone numbers. If you'd like to tweak Phone Formatter to better suit your situation, please do!
+
+**Brief Style Guide**: Two spaces, no space before function parentheses, semi-colons everywhere.
+
+## Release History
+
+* 0.0.1: Just two methods and some tests.
+
+## License
+Copyright (c) 2013 Steve Kinney  
+Licensed under the MIT license.
---
+++ test/test.js
@@ -0,0 +1,129 @@
+var assert = require('assert');
+var PhoneFormatter = require('../')
+
+describe('Normalize', function() {
+  
+  var testFormats = [
+  	"(212) 555 1212",
+  	"(212) 555.1212",
+  	"(212) 555-1212",
+  	"(212) 5551212",
+  	"(212)5551212",
+  	"212 555 1212",
+  	"212.555.1212",
+  	"212-555-1212",
+    "1-212-555-1212",
+    "+1 (212) 555-1212",
+    "12125551212",
+    "+45 (212) 555-1212",
+  	"2125551212"
+  ]
+  
+  testFormats.forEach(function(number) {
+    it('should be able to parse ' + number, function() {
+      assert.deepEqual(
+        PhoneFormatter.normalize(number),
+        "2125551212"
+      );
+    });
+  });
+  
+});
+
+describe('Format', function() {
+  
+  var phoneNumber = "2125551212"
+  
+  describe('strings', function () {
+    
+    it('should be able to format numbers with (NNN) NNN NNNN', function() {
+      assert.deepEqual(
+        PhoneFormatter.format(phoneNumber, "(NNN) NNN NNNN"),
+        "(212) 555 1212"
+      );
+    });
+  
+    it('should be able to format numbers with (NNN) NNN.NNNN', function() {
+      assert.deepEqual(
+        PhoneFormatter.format(phoneNumber, "(NNN) NNN.NNNN"),
+        "(212) 555.1212"
+      );
+    });
+  
+    it('should be able to format numbers with (NNN) NNN-NNNN', function() {
+      assert.deepEqual(
+        PhoneFormatter.format(phoneNumber, "(NNN) NNN-NNNN"),
+        "(212) 555-1212"
+      );
+    });
+  
+    it('should be able to format numbers with (NNN) NNNNNNN', function() {
+      assert.deepEqual(
+        PhoneFormatter.format(phoneNumber, "(NNN) NNNNNNN"),
+        "(212) 5551212"
+      );
+    });
+  
+    it('should be able to format numbers with (NNN)NNNNNNN', function() {
+      assert.deepEqual(
+        PhoneFormatter.format(phoneNumber, "(NNN)NNNNNNN"),
+        "(212)5551212"
+      );
+    });
+  
+    it('should be able to format numbers with NNN NNN NNNN', function() {
+      assert.deepEqual(
+        PhoneFormatter.format(phoneNumber, "NNN NNN NNNN"),
+        "212 555 1212"
+      );
+    });
+  
+    it('should be able to format numbers with NNN.NNN.NNNN', function() {
+      assert.deepEqual(
+        PhoneFormatter.format(phoneNumber, "NNN.NNN.NNNN"),
+        "212.555.1212"
+      );
+    });
+  
+    it('should be able to format numbers with NNN-NNN-NNNN', function() {
+      assert.deepEqual(
+        PhoneFormatter.format(phoneNumber, "NNN-NNN-NNNN"),
+        "212-555-1212"
+      );
+    });
+  
+    it('should be able to format numbers with 1-NNN-NNN-NNNN', function() {
+      assert.deepEqual(
+        PhoneFormatter.format(phoneNumber, "1-NNN-NNN-NNNN"),
+        "1-212-555-1212"
+      );
+    });
+  
+    it('should be able to format numbers with +1 (NNN) NNN-NNNN', function() {
+      assert.deepEqual(
+        PhoneFormatter.format(phoneNumber, "+1 (NNN) NNN-NNNN"),
+        "+1 (212) 555-1212"
+      );
+    });
+  
+    it('should be able to format numbers with 1NNNNNNNNNN', function() {
+      assert.deepEqual(
+        PhoneFormatter.format(phoneNumber, "1NNNNNNNNNN"),
+        "12125551212"
+      );
+    });
+    
+  });
+  
+  describe('options', function () {
+    
+    it('should be able to normalize a phone number before parsing it', function() {
+      assert.deepEqual(
+        PhoneFormatter.format("(212) 555-1212", "NNN.NNN.NNNN", {normalize: true}),
+        "212.555.1212"
+      );
+    });
+    
+  });
+  
+});
