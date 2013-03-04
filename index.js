e5c18093ab8a7eb75584008e6bf0411b9e6a9a28
Steve Kinney <hello@stevekinney.net>
2013-03-04 13:25:47 -0500

    Strip trailing whitespace and add options comment

--- index.js
+++ index.js
@@ -6,21 +6,22 @@ module.exports = {
   		/^[\+\d{1,3}\-\s]*\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
   		"$1$2$3"
   	);
-    
+
   },
 
   format: function format(phoneNumber, formatString, options) {
-    
+
+    // Normalize the phone number first if asked to do so in the options
     if (options && options.normalize === true) {
       phoneNumber = this.normalize(phoneNumber)
     };
-    
-  	for ( var i = 0, l = phoneNumber.length; i < l; i++ ) {
-  		formatString = formatString.replace("N", phoneNumber[i]);
-  	}
-    
+
+    for ( var i = 0, l = phoneNumber.length; i < l; i++ ) {
+      formatString = formatString.replace("N", phoneNumber[i]);
+    }
+
   	return formatString;
-    
+
   },
-  
+
 };
