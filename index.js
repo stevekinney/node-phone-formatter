8e786e8656a72dba348ccee586d6123734cc619d
Rob Loach <robloach@gmail.com>
2016-11-16 13:44:56 -0500

    Remove usage of 'this'

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
+function normalize(phoneNumber) {
+  return phoneNumber.replace(
+    /^[\+\d{1,3}\-\s]*\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
+    "$1$2$3"
+  );
+}
+
+function format(phoneNumber, formatString, options) {
+  // Normalize the phone number first unless not asked to do so in the options
+  if (!options || !options.normalize) {
+    phoneNumber = normalize(phoneNumber)
+  };
+  for ( var i = 0, l = phoneNumber.length; i < l; i++ ) {
+    formatString = formatString.replace("N", phoneNumber[i]);
+  }
+
+  return formatString;
+}
+
 module.exports = {
-  
-  normalize: function extract(phoneNumber) {
-
-    return phoneNumber.replace(
-      /^[\+\d{1,3}\-\s]*\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
-      "$1$2$3"
-    );
-
-  },
-
-  format: function format(phoneNumber, formatString, options) {
-
-    // Normalize the phone number first unless not asked to do so in the options
-    if (!options || !options.normalize) {
-      phoneNumber = this.normalize(phoneNumber)
-    };
-
-    for ( var i = 0, l = phoneNumber.length; i < l; i++ ) {
-      formatString = formatString.replace("N", phoneNumber[i]);
-    }
-  
-    return formatString;
-
-  },
-
+  normalize: normalize,
+  format: format
 };
